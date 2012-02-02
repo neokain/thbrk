d4c9007aa9b0b8859ce9132358b7a07acd2c95b1
Phuwanart Larpmark <neokain@gmail.com>
2012-02-02 14:20:59 +0700

    Ignore Iconv::IllegalSequence

--- lib/thbrk/core_ext.rb
+++ lib/thbrk/core_ext.rb
@@ -7,11 +7,11 @@ require 'libthai'
 String.class_eval do
   def to_thbrk
 
-    utf8_to_tis620 = Iconv.new("TIS620", "UTF-8")
-    a = LibThai::brk_line(utf8_to_tis620.iconv(self))
+    utf8_to_tis620 = Iconv.new("TIS620//IGNORE", "UTF-8")
+    a = LibThai::brk_line(utf8_to_tis620.iconv(self).join(""))
 
-    tis620_to_utf8 = Iconv.new("UTF-8", "TIS620")
-    o = tis620_to_utf8.iconv(a)
+    tis620_to_utf8 = Iconv.new("UTF-8//IGNORE", "TIS620")
+    o = tis620_to_utf8.iconv(a).join("")
 
     return o
   end
--- thbrk.gemspec
+++ thbrk.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'thbrk'
-  s.version     = '0.0.4'
+  s.version     = '0.0.5'
   s.date        = '2012-01-25'
   s.summary     = "Thbrk"
   s.description = "Thai word break"
